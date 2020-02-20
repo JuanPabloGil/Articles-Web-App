2c8ad07c66f828bd3a12715f118059a30f94c5f8
Juan Pablo <jpablomgil@gmail.com>
2020-02-20 11:51:03 -0600

    Define methods to create a new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,24 @@
 class ArticlesController < ApplicationController
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @user = User.all.first
+    @article = @user.articles.new(post_params)
+
+    if @article.save
+      redirect_to root_path
+      flash[:success] = 'Article created succesfully'
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:article).permit(:text, :title)
+  end
 end
