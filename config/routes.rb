1127c95c5802526ac01990f6a5623c29c8c73b8f
Juan Pablo <jpablomgil@gmail.com>
2020-02-20 15:54:34 -0600

    Created routes for login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :users
   resources :articles
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
