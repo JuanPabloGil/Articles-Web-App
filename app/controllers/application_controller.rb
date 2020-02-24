3d6994e01ef7a3509a68789702ce03ad527f51c9
Juan Pablo <jpablomgil@gmail.com>
2020-02-23 20:20:48 -0600

    set a default article

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < ActionController::Base
 
   def most_voted_article
     articles = Article.all
-    best_count = Article.all.first.votes.size
+    best_count = 0
     best_article = Article.all.first
     articles.each do |article|
       if article.votes.size > best_count
