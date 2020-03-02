93edb902006629cdc8bb5cd68c63cac747c0bc3e
Juan Pablo <jpablomgil@gmail.com>
2020-03-02 09:58:57 -0600

    Pas style as a parameter on vote_or_unvote

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
-  def vote_or_unvote(article)
+  def vote_or_unvote(article, style = 'label-form')
     if logged_in?
       vote = Vote.find_by(article: article, user: current_user)
       if vote
         link_to (fa_icon 'thumbs-o-down'), article_vote_path(id: vote.id, article_id: article.id),
-                class: 'label-form', method: :delete
+                class: style, method: :delete
       else
-        link_to (fa_icon 'thumbs-o-up '), article_votes_path(article_id: article.id), class: 'label-form', method: :post
+        link_to (fa_icon 'thumbs-o-up '), article_votes_path(article_id: article.id), class: style, method: :post
       end
     else
       link_to('Must be logged in to vote', login_path)
